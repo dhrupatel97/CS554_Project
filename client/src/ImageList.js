d25ff1854b011738e49c67b7e7c31e8cdb79ead1
TP170996 <55114902+TP170996@users.noreply.github.com>
2020-11-29 14:17:32 -0500

    Merged frontend with client

---
+++ client/src/ImageList.js
@@ -0,0 +1,183 @@
+export default Image=[
+    {
+        ID:1,
+        PHOTO_NAME: "https://images.unsplash.com/photo-1552793494-111afe03d0ca?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1950&q=80",
+        DESCRIPTION:"office image",
+        POSTED_BY: "tejashree",
+        CATEGORY: "office", 
+        NO_OF_LIKES:"5",
+        NO_OF_DISLIKES:"2",
+        KEYWORDS:["office","table"],
+        COMMENTS:[
+            {
+              USERNAME:"tejashree",
+              COMMENT:"Beautiful"
+            }
+
+        ]
+
+    },
+    {
+        ID:2,
+        PHOTO_NAME: "https://images.unsplash.com/photo-1566836610593-62a64888a216?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=970&q=80",
+        DESCRIPTION:"home image",
+        POSTED_BY: "tejashree",
+        CATEGORY: "home", 
+        NO_OF_LIKES:"4",
+        NO_OF_DISLIKES:"2",
+        KEYWORDS:["home","plants"],
+        COMMENTS:[
+            {
+              USERNAME:"tejashree",
+              COMMENT:"Beautiful"
+            }
+
+        ]
+
+    },
+    {
+        ID:3,
+        PHOTO_NAME: "https://images.unsplash.com/photo-1519331379826-f10be5486c6f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80",
+        DESCRIPTION:"outdoor image",
+        POSTED_BY: "tejashree",
+        CATEGORY: "outdoor", 
+        NO_OF_LIKES:"3",
+        NO_OF_DISLIKES:"2",
+        KEYWORDS:["outdoor","snow","trees"],
+        COMMENTS:[
+            {
+              USERNAME:"tejashree",
+              COMMENT:"Beautiful"
+            }
+
+        ]
+
+    },
+    {
+        ID:4,
+        PHOTO_NAME: "https://images.unsplash.com/photo-1447752875215-b2761acb3c5d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80",
+        DESCRIPTION:"outdoor image",
+        POSTED_BY: "tejashree",
+        CATEGORY: "outdoor", 
+        NO_OF_LIKES:"5",
+        NO_OF_DISLIKES:"2",
+        KEYWORDS:["outdoor","bridge"],
+        COMMENTS:[
+            {
+              USERNAME:"tejashree",
+              COMMENT:"Beautiful"
+            }
+
+        ]
+
+    },
+    {
+        ID:5,
+        PHOTO_NAME: "https://images.unsplash.com/photo-1497366811353-6870744d04b2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80",
+        DESCRIPTION:"office image",
+        POSTED_BY: "tejashree",
+        CATEGORY: "office", 
+        NO_OF_LIKES:"5",
+        NO_OF_DISLIKES:"2",
+        KEYWORDS:["office","table","conference"],
+        COMMENTS:[
+            {
+              USERNAME:"tejashree",
+              COMMENT:"Beautiful"
+            }
+
+        ]
+
+    },
+    {
+        ID:6,
+        PHOTO_NAME: "https://images.unsplash.com/photo-1443397646383-16272048780e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=967&q=80",
+        DESCRIPTION:"outdoor image",
+        POSTED_BY: "tejashree",
+        CATEGORY: "outdoor", 
+        NO_OF_LIKES:"5",
+        NO_OF_DISLIKES:"2",
+        KEYWORDS:["outdoor","ocean"],
+        COMMENTS:[
+            {
+              USERNAME:"tejashree",
+              COMMENT:"Beautiful"
+            }
+
+        ]
+
+    },
+    {
+        ID:7,
+        PHOTO_NAME: "https://images.unsplash.com/photo-1505691938895-1758d7feb511?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80",
+        DESCRIPTION:"home image",
+        POSTED_BY: "tejashree",
+        CATEGORY: "home", 
+        NO_OF_LIKES:"5",
+        NO_OF_DISLIKES:"2",
+        KEYWORDS:["home","sofa"],
+        COMMENTS:[
+            {
+              USERNAME:"tejashree",
+              COMMENT:"Beautiful"
+            }
+
+        ]
+
+    },
+    {
+        ID:8,
+        PHOTO_NAME: "https://images.unsplash.com/photo-1549637642-90187f64f420?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1053&q=80",
+        DESCRIPTION:"office image",
+        POSTED_BY: "tejashree",
+        CATEGORY: "office", 
+        NO_OF_LIKES:"5",
+        NO_OF_DISLIKES:"2",
+        KEYWORDS:["office","table","cubical"],
+        COMMENTS:[
+            {
+              USERNAME:"tejashree",
+              COMMENT:"Beautiful"
+            }
+
+        ]
+
+    },
+    {
+        ID:9,
+        PHOTO_NAME: "https://images.unsplash.com/photo-1519710164239-da123dc03ef4?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=334&q=80",
+        DESCRIPTION:"home image",
+        POSTED_BY: "tejashree",
+        CATEGORY: "home", 
+        NO_OF_LIKES:"5",
+        NO_OF_DISLIKES:"2",
+        KEYWORDS:["home","table"],
+        COMMENTS:[
+            {
+              USERNAME:"tejashree",
+              COMMENT:"Beautiful"
+            }
+
+        ]
+
+    },
+    {
+        ID:10,
+        PHOTO_NAME: "https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1078&q=80",
+        DESCRIPTION:"outdoor image",
+        POSTED_BY: "tejashree",
+        CATEGORY: "outdoor", 
+        NO_OF_LIKES:"5",
+        NO_OF_DISLIKES:"2",
+        KEYWORDS:["outdoor","mountains"],
+        COMMENTS:[
+            {
+              USERNAME:"tejashree",
+              COMMENT:"Beautiful"
+            }
+
+        ]
+
+    }
+
+]
