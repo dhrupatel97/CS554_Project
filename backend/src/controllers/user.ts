ad75ed293bb0f1ebc37e26192eaf7fba0070eb12
Meghana Bhat <mmeghanabhat@gmail.com>
2020-12-14 03:41:55 -0500

    Like Image Connection

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -2,16 +2,29 @@ import UserData from '../schema/userschema'
 import { Request, Response } from 'express';
 
 export class Users {
-    public routes(app): void {  
-          app.route('/api/users').post((req: Request, res: Response) => {  
-            let user = new UserData(req.body);
-            user.save((err: any) => {
-                if (err) {
-                  res.status(400).send(err.message);
-                } else {
-                  res.json(user);
-                }
-              });
-          });
+  public routes(app): void {
+    app.route('/api/users').post((req: Request, res: Response) => {
+      let user = new UserData(req.body);
+      user.save((err: any) => {
+        if (err) {
+          res.status(400).send(err.message);
+        } else {
+          res.json(user);
+        }
+      });
+    });
+
+    app.route('/api/users').get((req: Request, res: Response) => {
+      const currentUser = req['currentUser'];
+      UserData.findOne({
+        email: currentUser.email
+      }, function (err, user) {
+        if (err) {
+          res.status(500).send(err);
+        }else{
+          res.status(200).send(user)
+        }       
+      });
+    });
     }
 }
