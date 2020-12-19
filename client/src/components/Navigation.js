b2b986055b5f1361e1263117cf0c40eaea90cdbb
Meghana Bhat <mmeghanabhat@gmail.com>
2020-12-19 15:15:39 -0500

    cleaning up

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -2,10 +2,8 @@ import React, { useContext } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { AuthContext } from '../firebase/Auth';
 import SignOutButton from './SignOut';
-import {DropdownButton, Dropdown} from 'react-bootstrap'
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import '../App.css';
 
 const Navigation = () => {
@@ -16,76 +14,6 @@ const Navigation = () => {
 const NavigationAuth = () => {
   return (
     <div>
-    {/*<nav class="navbar navbar-expand-lg navbar-dark color">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="/">ARTSY</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-            {/*</li> <DropdownButton
-            //       alignRight
-            //       title = "Category"
-            //       variant="secondary"
-            //       id="dropdown-menu-align-right"
-                  
-                  
-            //     >
-            //       <Dropdown.Item ><Link exact to="/home">
-            //     Home
-            //   </Link></Dropdown.Item>
-            //       <Dropdown.Item ><Link exact to="/outdoor">
-            //     Outdoor
-            //   </Link></Dropdown.Item>
-            //       <Dropdown.Item ><Link exact to="/office">
-            //     Office
-            //   </Link></Dropdown.Item>
-            //   <Dropdown.Item ><Link exact to="/abstract">
-            //    Abstract
-            //   </Link></Dropdown.Item>
-            //   <Dropdown.Item ><Link exact to="/other">
-            //     Other
-            //   </Link></Dropdown.Item>
-              
-            //     </DropdownButton> */}{/*
-            </li>
-
-            <li class="nav-item">
-              <Link exact to="/hottest" class='nav-link' >
-                Hottest Backgrounds
-              </Link>
-            </li>
-            <li class="nav-item">
-              <Link exact to="/uploads" class='nav-link' >
-                My Photos
-              </Link>
-            </li>
-            <li class="nav-item">
-              <Link exact to="/uploadimage" class='nav-link'  >
-                Upload Image
-              </Link>
-            </li>
-          </ul>
-          <ul class='navbar-nav nav justify-content-end'>
-            <li class="nav-item">
-                  <p class='nav-link'>Welcome!</p>
-              </li>
-              <li class="nav-item">
-                <Link exact to="/account" class='nav-link' >
-                  Account
-                </Link>
-              </li>
-              <li class="nav-item">
-              <SignOutButton/>
-              </li>
-            </ul>
-        </div>
-      </div>
-            </nav> */}
-
-
   <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
   <Navbar.Brand href="/">ARTSY</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
